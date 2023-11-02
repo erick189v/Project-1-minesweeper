e479c14087f7349a4c6ff0d0f7ef8607bcc95cfa
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2023-11-02 14:15:23 -0400

    retrieves bomb adjencent"

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ let columns;
 
 //bombs
 let mines;
-let minesLocation;
+
+let minesLocation = [];
 
 //flag button
 let flagOn = false;
+let r;
+let c;
 //let gameOver =  false;
 
 
@@ -25,13 +28,13 @@ document.getElementById("flag-button").addEventListener("click",placeFlag)
 function makeGrid(){
 
     //populating with columns and rows
-    for(let i = 0; i < grid.length; i++){
-        for(let c = 0; c < grid[i].length; c++){
+    for(let r = 0; r < grid.length; r++){
+        for(let c = 0; c < grid[r].length; c++){
             //div tags created
             let newCell = document.createElement('div');
             //coordinates
             newCell.setAttribute('data-c',c);
-            newCell.setAttribute('data-r',i);
+            newCell.setAttribute('data-r',r);
             newCell.classList.add('cell')
             
             board.append(newCell)
@@ -48,16 +51,22 @@ function init(){
     
 
     grid = [
-        ["*","",""],
-        ["","*","*"],
-        ["","",""],
+        ["*","","","","","","","","",""],
+        ["","","","","*","","","","",""],
+        ["","","","","","","","*","",""],
+        ["","","*","*","","","","","",""],
+        ["","","","","*","","","","*",""],
+        ["","*","","","","","","","",""],
+        ["","","*","","","","","*","",""],
+        ["","","","","","","","","",""],
     ]
     //new cell clicked
     makeGrid()
-    board.removeEventListener('click', checkCell)
-    board.addEventListener('click',checkCell)
+    board.removeEventListener('dblclick', checkCell)
+    board.addEventListener('dblclick',checkCell)
 
     board.addEventListener('click', flagCell)
+
 
     
 
@@ -75,17 +84,61 @@ minesLoc("💥")
 function checkCell(e){
     //console.log(e.target.dataset)
     //console.dir(e.target)
-    console.log(e.target.getAttribute("data-r"))
-    console.log(e.target.getAttribute("data-c"))
-    console.log(grid[e.target.getAttribute("data-r")][e.target.getAttribute("data-c")])
-    
+    let r = e.target.getAttribute("data-r") * 1
+    let c = e.target.getAttribute("data-c") * 1
+    console.log(grid[r][c]) 
+    if(grid[r][c]){
+         gameOver();
 
-   /* if(r-1 >=0){
+    }
+
+    let minesNear = 0;
+    
+    console.log('topLeft',grid[r-1][c-1])
+    console.log('topRight',grid[r-1][c+1])
+    console.log(typeof r);
+    console.log('top',grid[r-1][c])
+    console.log('bottom', grid[r+1][c])
+    console.log('BottomLeft',grid[r+1][c-1])
+    console.log('BottomRight',grid[r+1][c+1])
+    console.log('Right',grid[r][c+1])
+    console.log('Left',grid[r][c-1])
+
+   if (grid[r-1][c-1]){
+    minesNear++
+   } 
+   if (grid[r-1][c+1]){
+    minesNear++
+   } 
+   if (grid[r-1][c]){
+    minesNear++
+   } 
+   if (grid[r+1][c]){
+    minesNear++
+   } 
+   if (grid[r+1][c-1]){
+    minesNear++
+   } 
+   if (grid[r+1][c+1]){
+    minesNear++
+   } 
+   if (grid[r][c+1]){
+    minesNear++
+   } 
+   if (grid[r][c-1]){
+    minesNear++
+   } 
+
+   
+   console.log(minesNear)
+   return minesNear
+    
+    /* if(r-1 >=0){
         const neighbor = grid[r-1][c]
         console.log(neighbor)
         if(neighbor ===""){
             grid[c][r] = "0"
-          return false
+            return false
         } else if (neighbor == "*"){
             return true
           }
@@ -96,15 +149,22 @@ function checkCell(e){
     //console.log(e.target)
 }
 
+//game over and it resets.
+function gameOver(){
+    alert('GAME OVER 💥')
+    location.reload()
+}
+
+//places a flag when toggled and  with a single click
 function placeFlag(){
 
     if (flagOn){
         flagOn = false;
-        document.getElementById("flag-button").style.backgroundColor = "#84b78a";
+        document.getElementById("flag-button").style.backgroundColor = "#f1bce8";
     }
     else {
         flagOn = true;
-        document.getElementById("flag-button").style.backgroundColor = "#f1bce8";
+        document.getElementById("flag-button").style.backgroundColor = "#84b78a";
     }
 }
 
@@ -122,16 +182,12 @@ function flagCell(e){
         }
         return;
     }
-    //if(minesLoc.includes(newCell.id)){
-       // alert("GAME OVER")
-        //gameOver = true;
-        //return;
-    //}
     
 }
 
-//checking for bombs
-function checkAbove(r,c){
+
+
+/*function checkMines(r,c){
     if(r-1 >=0){
         const neighbor = grid[r-1][c]
         console.log(neighbor)
@@ -144,6 +200,15 @@ function checkAbove(r,c){
     } else {
         return false
     }
-}
-checkAbove(1, 0)
+}*/
+
+// function checkMines(r,c,e){
+//     if (r < 0 || r >= rows || c < 0 || c >= columns){
+//         return 0;
+//     }
+//     if(minesLocation.includes(grid[e])){
+//         return 1;
+//     }
+//     return 0;
+// }
 //console.log(grid);
